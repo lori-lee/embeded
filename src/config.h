e8cf02c6812bd463b4d76af030c68f83bdc28f5f
Lori Lee <leejqy@sina.com>
2015-08-12 18:23:06 +0800

    update

--- src/config.h
+++ src/config.h
@@ -22,20 +22,29 @@
 
 #if !defined __CONFIG_H__
 #define __CONFIG_H__
+//INT Source
+#define  INT_TIMER0 0x1
+#define  INT_LSENOR 0x2
+#define  INT_RTCTR  0x4
+#define  INT_SWITCH 0x8
+/**
+ * Current system status
+ *
+ **/
 typedef struct _sys_status {
-    bit bdata mode;//0 --auto mode, 1 --manual mode
-    bit bdata e_relay;//0 --relay off, 1 --relay on
-    bit bdata h_sensor;//0 --no human detected, 1 -- the opposite
-    bit bdata wdt;//0 -- watch dog disabled, 1 -- enabled
-    bit bdata bcontrol;//
+    unsigned char mode: 1;//0 --auto mode, 1 --manual mode
+    unsigned char e_relay: 1;//0 --relay off, 1 --relay on
+    unsigned char h_sensor: 1;//0 --no human detected, 1 -- the opposite
+    unsigned char wdt: 1;//0 -- watch dog disabled, 1 -- enabled
     //
-    bit bdata second_flag;//1 second ?
-    bit bdata manual_save_flag;//SAVE from Remote control?
+    unsigned char second_flag: 1;//1 second ?
+    unsigned char manual_save_flag: 1;//SAVE from Remote control?
 
-    unsigned char data int_source;//0x1 --timer0, 0x2 --light intensity sensor, 0x4 -- remote control
-    unsigned int data l_sensor;//the value of light intensity
-    unsigned int data remote_control;//
+    unsigned char int_source;//0x1 --timer0, 0x2 --light intensity sensor, 0x4 -- remote control, 0x8 -- Switch
+    unsigned int l_sensor;//the value of light intensity
     unsigned int secs_elapsed;//seconds elapsed in manual mode
+    unsigned char hour;//for auto turn off bulk when timeout
+    unsigned char minute;//for auto turn off bulk when timeout
 }sys_status;
 //
 volatile sys_status data g_sysstatus;
@@ -49,9 +58,12 @@ volatile sys_status data g_sysstatus;
 #define mark_remote_control_status(value)   g_sysstatus.remote_control = value
 #define mark_second_flag_status(value)      g_sysstatus.second_flag    = value
 #define mark_manual_save_flag_status(value) g_sysstatus.manual_save_flag = value
-#define mark_secs_elapsed(value)            g_sysstatus.secs_elapsed   = value
+#define mark_secs_elapsed_status(value)     g_sysstatus.secs_elapsed   = value
+#define mark_hour_status(value)             g_sysstatus.hour  = value
+#define mark_minute_status(value)           g_sysstatus.minute= value
 
 #define mark_status(member,value)   mark_##member##_status (value)
+#define clear_int_source()          g_sysstatus.int_source = 0
 
 #define get_mode_status()             g_sysstatus.mode
 #define get_e_relay_status()          g_sysstatus.e_relay
@@ -60,31 +72,45 @@ volatile sys_status data g_sysstatus;
 #define get_int_source_status()       g_sysstatus.int_source
 #define get_l_sensor_status()         g_sysstatus.l_sensor
 #define get_remote_control_status()   g_sysstatus.remote_control
-#define get_secs_elapsed()            g_sysstatus.secs_elapsed
+#define get_secs_elapsed_status()     g_sysstatus.secs_elapsed
+#define get_hour_status()             g_sysstatus.hour
+#define get_minute_status()           g_sysstatus.minute
+
 #define get_status(member) get_##member##_status ()
 
 #define is_relay_on() get_status (e_relay)
 #define is_relay_off() !get_status (e_relay)
 #define need_save_config() get_status (manual_save_flag)
 
+/**
+ *
+ * System configuration
+ *
+ **/
 typedef struct _sys_config {
     unsigned int light_threshold;//min light intensity
     unsigned int m2a_mode_threshold;//timeout (seconds) before switch manual mode to auto-mode
+    unsigned char hour;//
+    unsigned char minute;//
 }sys_config;
 //
 volatile sys_config data g_sysconfig;
 
 #define mark_light_threshold_config(value)       g_sysconfig.light_threshold    = value
 #define mark_m2a_mode_threshold_config(value)    g_sysconfig.m2a_mode_threshold = value
+#define mark_hour_config(value)                  g_sysconfig.hour  = value
+#define mark_minute_config(value)                g_sysconfig.minute= value
 
 #define mark_config(member,value) mark_##member##_config (value)
 
 #define get_light_threshold_config()       g_sysconfig.light_threshold
 #define get_m2a_mode_threshold_config()    g_sysconfig.m2a_mode_threshold
+#define get_hour_config()                  g_sysconfig.hour
+#define get_minute_config()                g_sysconfig.minute
 
 #define get_config(member) mark_##member##_config ()
 
-#define is_night()      (get_status (l_sensor) < get_config (light_threshold))
+#define is_dark()       (get_status (l_sensor) < get_config (light_threshold))
 #define is_automode()   (0 == get_status (mode))
 #define is_manualmode() (!is_automode ())
 
@@ -98,5 +124,5 @@ volatile sys_config data g_sysconfig;
 }while (0)
 
 
-void init_config (void);
+extern void init_config (void);
 #endif
