b9778ec6351ed4d62e806c48539b3a75fed42937
Lori Lee <leejqy@sina.com>
2015-08-13 17:15:37 +0800

    update

--- src/eprom.c
+++ src/eprom.c
@@ -1,3 +1,54 @@
+#include <intrins.h>
+#include "eprom.h"
+
+void turn_off_iap()
+{
+    IAP_CONTR = 0;
+    IAP_CMD = 0;
+    IAP_TRIG= 0;
+    IAP_ADDRH = 0x80;
+    IAP_ADDRL = 0x0;
+}
+
+bit write_byte (unsigned char byte, unsigned int addr)
+{
+    bit b;
+
+    if (!can_iap ()) return 0;//Should not operate EEPROM when Voltage too low
+    enable_iap ();
+    IAP_DATA = byte;
+    IAP_ADDRH= addr >> 8;
+    IAP_ADDRL= addr << 8;
+    IAP_CMD  = IAP_CMD_WT;
+
+    execute_iap (WT);
+    b = is_iap_fail ();
+    turn_off_iap ();
+
+    return ~b;
+}
+
+unsigned char read_byte (unsigned int addr)
+{
+    unsigned char byte;
+    bit b;
+
+    if (!can_iap ()) return 0;//Should not operate EEPROM when Voltage too low
+
+    enable_iap ();
+    IAP_ADDRH= addr >> 8;
+    IAP_ADDRL= addr << 8;
+    IAP_CMD  = IAP_CMD_RD;
+
+    execute_iap (RD);
+    
+    byte = IAP_DATA;
+    b = is_iap_fail ();
+    turn_off_iap ();
+
+    return byte;
+}
+
 void save_sys_config (void)
 {
 }
--- src/eprom.h
+++ src/eprom.h
@@ -1,5 +1,28 @@
 #if !defined __EPROM_H__
 #define __EPROM_H__
 
+#include "config.h"
+//Idle mode
+#define IAP_CMD_ST  0
+//Read by byte
+#define IAP_CMD_RD  1
+//Write by byte
+#define IAP_CMD_WT  2
+//Erase sector
+#define IAP_CMD_ER  3
+
+#define delay(n) for(int i = 0; i < n; ++i) _nop_();
+#define CYCLES_RD 2
+#define CYCLES_WT 1320
+
+#define can_iap() get_status (low_v)
+#define enable_iap() IAP_CONTR = 0x80
+#define disable_iap() IAP_CONTR&= 0x7F
+#define execute_iap(type) do {IAP_TRIG = 0x5A; IAP_TRIG = 0xA5;delay (CYCLES_##type);}while(0)
+
+#define is_iap_fail() IAP_CONTR &= ~0x10
+static void turn_off_iap (void);
+
+extern bit write_byte (unsigned char byte, unsigned int addr);
 extern void save_sys_config (void);
 #endif
