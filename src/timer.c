e1bb18b042536c42ddeda5f176cde7b4948aaf47
Lori Lee <leejqy@sina.com>
2015-08-13 13:09:11 +0800

    update

--- src/timer.c
+++ src/timer.c
@@ -12,8 +12,7 @@ void timer0_ISR (void) interrupt 1 using 0
     run_timer (0);
     //enable_int ();
     //
-    --counter;
-    if (!counter) {//1 second elapsed ?
+    if (!--counter) {//1 second elapsed ?
         counter = 0x14;
         mark_status (second_flag, 1);
         mark_status (int_source, get_status (int_source) | INT_TIMER0);
